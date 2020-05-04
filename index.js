e67c293fd9d42b827b0848590e51ddb53a4b2c75
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-04 15:33:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+function updateObjectWithKeyAndValue(object, key, value){
+  var result = Object.assign({}, object);
+  result[key] = value;
+  return result;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] =  value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var result = Object.assign({}, object);
+  delete result[key];
+  return result;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
